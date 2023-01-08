03ab73d3584aee59d1111f3bdad07040a9a77292
n0893357 <n0893357@ntu.ac.uk>
2023-01-08 21:10:43 +0000

    war added

--- src/main/java/net/sccc/springboot/TravelBuddyBackendApplication.java
+++ src/main/java/net/sccc/springboot/TravelBuddyBackendApplication.java
@@ -1,13 +1,21 @@
 package net.sccc.springboot;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class TravelBuddyBackendApplication {
+public class TravelBuddyBackendApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TravelBuddyBackendApplication.class, args);
 	}
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	        return builder.sources(TravelBuddyBackendApplication.class);
+	    }
 
 }
